7ce52bba67b3a68b2c8e98c856b69b5a91f51bcf
TJ Stifter <tjstifter@gmail.com>
2023-05-26 15:18:30 -0400

    completes lab

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 const init = () => {
-  
+  const form = document.querySelector('form');
+  form.addEventListener('submit', (e)=> {
+    e.preventDefault();
+    const myInput = e.target['searchByID'];
+    console.log(myInput.value);
+
+    fetch(`http://localhost:3000/movies/${myInput.value}`)
+    .then ( r => r.json())
+    .then (movieData => renderData(movieData))
+  });
+}
+
+function renderData(movie){
+    const title = document.querySelector('section#movieDetails h4');
+    const summary = document.querySelector('section#movieDetails p');
+    title.textContent = movie.title;
+    summary.textContent = movie.summary;
 }
 
 document.addEventListener('DOMContentLoaded', init);
